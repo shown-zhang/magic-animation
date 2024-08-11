f72fdbcbd9427cfa48e310975cae1f1b6f9915ce
shown zhang <shownstudio2u@gmail.com>
2024-08-12 07:22:38 +0800

    chore: init

---
+++ src/main.ts
@@ -0,0 +1,52 @@
+const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+
+canvas.width = 500;
+canvas.height = 500;
+
+const gl = canvas.getContext('webgl');
+
+if (!gl) {
+  throw new Error('WebGL not supported');
+}
+
+const vertex = `
+  attribute vec2 position;
+  varying vec3 color;
+
+  void main() {
+    gl_PointSize = 1.0;
+    color = vec3(0.5 + position * 0.5, 0.0);
+    gl_Position = vec4(position * 0.5, 1.0, 1.0);
+  }
+`;
+
+const fragment = `
+  precision mediump float;varying vec3 color;void main(){ gl_FragColor = vec4(color, 1.0);} 
+`;
+
+const vertexShader = gl.createShader(gl.VERTEX_SHADER) as WebGLShader;
+gl.shaderSource(vertexShader, vertex);
+gl.compileShader(vertexShader);
+
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER) as WebGLShader;
+gl.shaderSource(fragmentShader, fragment);
+gl.compileShader(fragmentShader);
+
+const program = gl.createProgram() as WebGLProgram;
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+gl.linkProgram(program);
+
+gl.useProgram(program);
+
+const points = new Float32Array([-1, -1, 0, 1, 1, -1]);
+const bufferId = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+gl.bufferData(gl.ARRAY_BUFFER, points, gl.STATIC_DRAW);
+
+const vPosition = gl.getAttribLocation(program, 'position'); // 获取顶点着色器中的position变量的地址
+gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0); // 给变量设置长度和类型
+gl.enableVertexAttribArray(vPosition); // 激活这个变量
+
+gl.clear(gl.COLOR_BUFFER_BIT);
+gl.drawArrays(gl.TRIANGLES, 0, points.length / 2);
